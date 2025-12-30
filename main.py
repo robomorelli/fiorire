21c29788e5eb5c024c4223b789fd1658c983ff51
rob <roberto.morelli7@unibo.it>
2025-12-30 15:42:08 +0100

    comment in english

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--address", default='10.141.1.28:6379', help="address of master")
     parser.add_argument("--password", help="password to connect to master")
-    parser.add_argument("--config_file", default='conv_ae2D', help="[conv_ae1D, conv_ae2D, lstm]")
+    parser.add_argument("--config_file", default='conv_ae1D', help="[conv_ae1D, conv_ae2D, lstm]")
     parser.add_argument("--num_samples", default=100, help="the model you want to hpo")
     parser.add_argument("--wandb", default=0, type=int, help="the model you want to hpo")
     parser.add_argument("--project_name", default='fiorire1_1D', help="the model you want to hpo")
