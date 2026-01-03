7437dcb43324fb07cf2cd7a1229a0912a818a227
roberto.morelli.ext <roberto.morelli.ext@leonardo.com>
2026-01-03 17:36:15 +0100

    CONFIG FREEZE IS NOT WORKING

--- launch_hpo.sh
+++ launch_hpo.sh
@@ -245,8 +245,8 @@ ssh $MASTER_NODE "
   [[ -n \"$WANDB_KEY\" ]] && CMD+=\" --wandb_key $WANDB_KEY\"
   [[ -n \"$ENTITY\" ]] && CMD+=\" --entity $ENTITY\"
   [[ -n \"$WANDB\" ]] && CMD+=\" --wandb $WANDB\"
-  [[ -n \"$PROJECT_NAME\" ]] && CMD+=\" --project_name $PROJECT_NAME\
-  [[ -n \"$NUM_GPUS\" ]] && CMD+=\" --n_gpus $NUM_GPUS\
+  [[ -n \"$PROJECT_NAME\" ]] && CMD+=\" --project_name $PROJECT_NAME\"
+  [[ -n \"$NUM_GPUS\" ]] && CMD+=\" --n_gpus $NUM_GPUS\"
   [[ -n \"$NUM_CPUS\" ]] && CMD+=\" --n_cpus $NUM_CPUS\"
 
   echo \"\"
