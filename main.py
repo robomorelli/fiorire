fd2ff0f19244b3e14f1e645805adf7192a0672ee
roberto.morelli.ext <roberto.morelli.ext@leonardo.com>
2026-01-19 18:23:03 +0100

    merging refactoring_conv into main

--- main.py
+++ main.py
@@ -389,7 +389,7 @@ if __name__ == "__main__":
                             help="Ray head node address (default: None for local cluster)")
         parser.add_argument("--password", default=None,
                             help="Ray cluster password")
-        parser.add_argument("--config_file", "-c", default='conv_ae2D_AOC',
+        parser.add_argument("--config_file", "-c", default='conv_ae1D_ESA',
                             help="Config file name")
         parser.add_argument("--trials_per_node", default=1, type=int,
                             help="trial per node")
